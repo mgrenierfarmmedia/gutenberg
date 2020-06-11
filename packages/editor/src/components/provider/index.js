c4e7e34823aa19eda5a924fbce05da74fd6193de
Riad Benguella <benguella@gmail.com>
2020-06-11 14:04:48 +0100

    Add the browse all button

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -126,6 +126,7 @@ class EditorProvider extends Component {
 				'__experimentalGlobalStylesUserEntityId',
 				'__experimentalGlobalStylesBase',
 				'__experimentalPreferredStyleVariations',
+				'__experimentalOnExpandInserter',
 				'alignWide',
 				'allowedBlockTypes',
 				'availableLegacyWidgets',
